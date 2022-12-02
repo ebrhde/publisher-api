92c2ea06a6a4a61fe486027a2bd393869e53b092
ebrhde <tharuthlessman@gmail.com>
2022-12-02 20:54:48 +0400

    api documentation

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use App\Service\CategoryService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Model\CategoryApiResponse;
+use OpenApi\Annotations as OA;
+use Nelmio\ApiDocBundle\Annotation\Model;
 
 class CategoryController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
@@ -16,7 +19,12 @@ class CategoryController extends \Symfony\Bundle\FrameworkBundle\Controller\Abst
     }
 
     /**
-     * @Route("/api/v1/book/categories", name="app_categories")
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns book categories",
+     *     @Model(type=CategoryApiResponse::class)
+     * )
+     * @Route("/api/v1/book/categories", name="app_categories", methods="GET")
      */
     public function categories(): Response {
         return $this->json($this->categoryService->getCategories());
