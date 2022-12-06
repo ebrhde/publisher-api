23144b84de5dd3dd8931f5ef7e16ae061841b084
ebrhde <tharuthlessman@gmail.com>
2022-12-06 13:55:38 +0400

    Book service tests

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -65,6 +65,13 @@ class Book
         return $this->id;
     }
 
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
     public function getTitle(): ?string
     {
         return $this->title;
---
+++ tests/Service/BookServiceTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Book;
+use App\Entity\Category;
+use App\Exception\CategoryNotFoundException;
+use App\Model\BookApiResponse;
+use App\Model\BookApiResponseItem;
+use App\Repository\BookRepository;
+use App\Repository\CategoryRepository;
+use App\Service\BookService;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use PHPUnit\Framework\TestCase;
+
+class BookServiceTest extends TestCase
+{
+    public function testGetBooksByCategoryNotFound(): void
+    {
+        $bookRepository = $this->createMock(BookRepository::class);
+        $categoryRepository = $this->createMock(CategoryRepository::class);
+        $categoryRepository->expects($this->once())
+            ->method('find')
+            ->with(189)
+            ->willThrowException(new CategoryNotFoundException());
+
+        $this->expectException(CategoryNotFoundException::class);
+
+        (new BookService($bookRepository, $categoryRepository))->getBooksByCategory(189);
+    }
+
+    public function testGetBooksByCategory(): void
+    {
+        $bookRepository = $this->createMock(BookRepository::class);
+        $bookRepository->expects($this->once())
+            ->method('findBooksByCategoryId')
+            ->with(189)
+            ->willReturn([$this->createBookEntity()]);
+
+        $categoryRepository = $this->createMock(CategoryRepository::class);
+        $categoryRepository->expects($this->once())
+            ->method('find')
+            ->with(189)
+            ->willReturn(new Category());
+
+        $service = new BookService($bookRepository, $categoryRepository);
+        $expected = new BookApiResponse([$this->createBookItemModel()]);
+
+        $this->assertEquals($expected, $service->getBooksByCategory(189));
+    }
+
+    private function createBookEntity(): Book
+    {
+        return (new Book())
+            ->setId(123)
+            ->setTitle('Test Book')
+            ->setSlug('test-book')
+            ->setMeap(false)
+            ->setAuthors(['A. Tester'])
+            ->setImage('http://localhost/test.png')
+            ->setCategories(new ArrayCollection())
+            ->setPublicationDate(new DateTime('2020-10-10'));
+
+    }
+
+    private function createBookItemModel(): BookApiResponseItem
+    {
+        return (new BookApiResponseItem())
+            ->setId(123)
+            ->setTitle('Test Book')
+            ->setSlug('test-book')
+            ->setMeap(false)
+            ->setAuthors(['A. Tester'])
+            ->setImage('http://localhost/test.png')
+            ->setPublicationDate(1602288000);
+    }
+}
